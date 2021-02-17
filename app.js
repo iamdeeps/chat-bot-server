0fe6a6b618033f1bfbea48eb67711fc8d25d4f44
Deepak <deepakbehki@gmail.com>
2021-02-18 02:40:55 +0530

    communication dependencies added and tested apis

--- app.js
+++ app.js
@@ -2,24 +2,34 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept")
+  res.setHeader("Access-Control-Allow-Methods","Get, Post, Patch, Delete, Put, Options")
+  next()
+})
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET User Data */
+router.post('/login', function(req, res, next) {
+  console.log('req',req.body)
+  const userDetails = [
+    {id:'1',userName:"first login",password:'first pass'}
+  ]
+  if(userDetails.length){
+    return res.status(200).json({message:"User Data Fetched Successfully",userData:userDetails})
+  }
+  return res.status(400).json({message:"User not found",userData:userDetails})
 });
+
+/* Register User Data */
+router.post('/register',function (req, res, next){
+  console.log('req',req.body)
+  console.log("🚀 ~ file: index.js ~ line 21 ~ pass", pass)
+  res.status(201).json({message:"User not found"})
+})
+
+router.get('/users',function (req, res, next){
+  res.status(200).json({message:"Users sent",userData:''})
+})
 
 module.exports = router;
